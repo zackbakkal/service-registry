c8a8e27907f11b8ef0e9ad423d7c212b8808dc02
zackbakkal <zackbakkal@gmail.com>
2021-02-28 11:56:41 -0700

    Eureka discovery server Initial commit

---
+++ src/test/java/com/zack/projects/chatapp/service/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zack.projects.chatapp.service.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
